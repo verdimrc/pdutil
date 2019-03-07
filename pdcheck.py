d39fca5f2b942cac5ef4a946a2a24a634dce6c51
Verdi March <marcverd@amazon.com>
2019-03-07 21:28:25 +0800

    pdcheck.py: various improvements
    Changes:
    - check_all()
    - probe dtypes on non-nan values
    - serialize list of sample values to string, so that jupyter notebook can
      display paddings.

--- pdcheck.py
+++ pdcheck.py
@@ -1,6 +1,14 @@
+import json
 import pandas as pd
 
+def check_all(df, max_value_to_show=10):
+    dfs = [check_columns(df, max_value_to_show),
+           check_datapoints_dtype(df).iloc[:, 1:]]
+    return pd.concat(dfs, axis=1)
+
+
 def check_columns(df, max_item_to_show=10):
+    '''Column dtype are computed from non-NaN values to prevent int64 columns becomes float64.'''
     column = []
     dtyp = []
     uniq_cnt = []
@@ -15,18 +23,30 @@ def check_columns(df, max_item_to_show=10):
         col = df[i]
         uniques = col.unique()
         cnt = len(col)
-        
+
         column.append(i)
-        dtyp.append(col.dtype)
+        dtyp.append(col.dropna().dtype)
         uniq_cnt.append(len(uniques))
         nan_cnt.append(cnt - col.count())
         data_cnt.append(cnt)
-        sample_value.append(uniques[:max_item_to_show])
+
+        # Convert to string, otherwise jupyter notebook display without padding spaces
+        #sample_value.append(str(uniques[:max_item_to_show].tolist()))
+        sample_value.append(json.dumps(uniques[:max_item_to_show].tolist()))
 
     return pd.DataFrame(d, columns=['column', 'dtype', 'uniq_cnt', 'data_cnt', 'nan_cnt', 'sample_value'])
 
 
 def check_datapoints_dtype(df):
-    print('col, dtypes')
-    for i in df.columns:
-        print(i, df[i].apply(type).unique(), sep=', ')
+    '''Only dtypes of non-NaN values to prevent int64 columns become float64.'''
+    column = list(df.columns)
+    dtypes = []
+    dtype_cnt = []
+    d = {'column': column, 'dtypes': dtypes, 'dtype_cnt': dtype_cnt}
+
+    for i in column:
+        dt = df[i].dropna().apply(lambda x: x.__class__.__name__).unique().tolist()
+        dtypes.append(dt)
+        dtype_cnt.append(len(dt))
+
+    return pd.DataFrame(d, columns=['column', 'dtypes', 'dtype_cnt'])
