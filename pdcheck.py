79e663d359d8dd79edde9ee8c76b10c29bee2dd6
Verdi March <marcverd@amazon.com>
2019-03-07 15:20:02 +0800

    Check columns to return DataFrame

--- pdcheck.py
+++ pdcheck.py
@@ -1,11 +1,30 @@
+import pandas as pd
+
 def check_columns(df, max_item_to_show=10):
-    print(f'col, dtype, #uniques, nan/total, head({max_item_to_show})')
+    column = []
+    dtyp = []
+    uniq_cnt = []
+    data_cnt = []
+    nan_cnt=[]
+    sample_value = []
+    d = {'column': column, 'dtype': dtyp, 'uniq_cnt': uniq_cnt,
+         'data_cnt': data_cnt, 'nan_cnt': nan_cnt,
+         'sample_value': sample_value}
+
     for i in df.columns:
         col = df[i]
         uniques = col.unique()
         cnt = len(col)
-        nan_cnt = cnt - col.count()
-        print(i, col.dtype, len(uniques), f'{nan_cnt}/{cnt}', uniques[:max_item_to_show], sep=', ')
+        
+        column.append(i)
+        dtyp.append(col.dtype)
+        uniq_cnt.append(len(uniques))
+        nan_cnt.append(cnt - col.count())
+        data_cnt.append(cnt)
+        sample_value.append(uniques[:max_item_to_show])
+
+    return pd.DataFrame(d, columns=['column', 'dtype', 'uniq_cnt', 'data_cnt', 'nan_cnt', 'sample_value'])
+
 
 def check_datapoints_dtype(df):
     print('col, dtypes')
